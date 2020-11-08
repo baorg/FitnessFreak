03b16ba0e83b26129e0c692c80f4559b2e64d55d
MR.AV <akashvermaa0208@gmail.com>
2020-11-08 18:49:18 +0530

    create Databse

--- server/Users/model.js
+++ server/Users/model.js
@@ -1,14 +1,30 @@
 const findOrCreate = require("mongoose-findorcreate");
 const mongoose = require("mongoose");
 
+const tagsSchema = new mongoose.Schema({
+    upDown : [],
+    answer : String,
+    comments : [commentSchema],
+    quesId : {type: mongoose.Schema.Types.ObjectId, ref: 'Ques'},
+    userId : {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+});
+const Tag = new mongoose.model("Tag", tagsSchema);
 
 const quesSchema = new mongoose.Schema({
     upDown : [],
     answers : [{}],
     categoryName : String,
     userId : {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    tags : [{type: mongoose.Schema.Types.ObjectId, ref: 'Tag'}].limit(5)
 });
 const Ques = new mongoose.model("Ques", quesSchema);
+
+
+const commentSchema = new mongoose.Schema({
+    comment : String,
+    userId : {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+});
+const Comment = new mongoose.model("Comment", commentSchema);
 
 const answerSchema = new mongoose.Schema({
     upDown : [],
@@ -19,8 +35,6 @@ const answerSchema = new mongoose.Schema({
 });
 const Ans = new mongoose.model("Ans", answerSchema);
 
-
-
 const userSchema = new mongoose.Schema({
     userName: String,
     googleId: String,
@@ -28,6 +42,13 @@ const userSchema = new mongoose.Schema({
     ProfileImage : String,
     question : [{type: mongoose.Schema.Types.ObjectId, ref: 'Ques'}],
     answer : [{type: mongoose.Schema.Types.ObjectId, ref: 'Ans'}],
+    Bio : String,
+    bookmarks : [{type: mongoose.Schema.Types.ObjectId, ref: 'Ques'}],
+    followers : [String],
+    following : [String],
+    score : {totalScore : Number},
+    notifications : [],
+    attachments : [{url : String, type : String}].limit(5)
 });
 
 userSchema.plugin(findOrCreate);
@@ -36,5 +57,9 @@ const User = new mongoose.model("User", userSchema);
 module.exports = {
 
     User,
+    Ans, 
+    Ques,
+    Tag, 
+    Comment
     
 }
