e47d7c477ad0015b17e9b91cc5179d1a39e64f02
MR.AV <akashvermaa0208@gmail.com>
2020-11-29 18:24:05 +0530

    add score list in config

--- server/config/index.js
+++ server/config/index.js
@@ -2,5 +2,6 @@ module.exports = {
     CLIENT_DOMAIN: 'http://localhost:3000',
     API_DOMAIN: 'http://localhost:5000',
     key: require('./key'),
-    category : requrie('./category').category
+    category : requrie('./category').category,
+    score : requrie('./score').score
 }
