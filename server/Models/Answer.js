6856f3cfd3ad3cf32f1f4f8e50bae8748ac2b4a7
Mudit Bisht <3145159Noobi7uke@users.noreply.github.com>
2020-11-29 17:44:03 +0530

    vote_count added to Ques Model

--- server/Models/Answer.js
+++ server/Models/Answer.js
@@ -2,7 +2,14 @@ const mongoose = require("mongoose");
 const { commentSchema } = require("./Comment")
 
 const answerSchema = new mongoose.Schema({
-    upDown: [],
+    upDown: [{
+        value: Number,
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    }],
+    vote_count: { upvote: { type: Number, default: 0 }, downvote: { type: Number, default: 0 } },
     answer: String,
     comments: [commentSchema],
     quesId: { type: mongoose.Schema.Types.ObjectId, ref: 'Ques' },
