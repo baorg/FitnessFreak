1dcc8719d0765b64a0a6c17447a6b55141cf26b8
MR.AV <akashvermaa0208@gmail.com>
2020-11-29 18:28:39 +0530

    resolve require conflict

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,6 @@ module.exports = {
     CLIENT_DOMAIN: 'http://localhost:3000',
     API_DOMAIN: 'http://localhost:5000',
     key: require('./key'),
-    category : requrie('./category').category,
-    score : requrie('./score').score
+    category : require('./category').category,
+    score : require('./score').score
 }
