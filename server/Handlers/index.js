4f2fc0a411bb8fc38e5e5e013796cd9a18fb8e59
MR.AV <akashvermaa0208@gmail.com>
2020-12-04 21:06:52 +0530

    Add Ranking handler

--- server/Handlers/index.js
+++ server/Handlers/index.js
@@ -3,11 +3,12 @@ const FollowingHandler = require('./Following');
 const QuestionHandler = require('./Question');
 const UsersHandler = require('./Users');
 const FeedHandler = require('./Feed');
-
+const RankHandler = require('./Rank')
 module.exports = {
     AuthHandler,
     FollowingHandler,
     QuestionHandler,
     UsersHandler,
     FeedHandler,
+    RankHandler
 }
