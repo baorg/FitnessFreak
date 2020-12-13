b0032f3151ea579b7932c26c78b6066bacb8ed8b
MR.AV <akashvermaa0208@gmail.com>
2020-12-13 19:25:01 +0530

    Marked Answer Handler

--- server/Handlers/Question/markAnswer.js
+++ server/Handlers/Question/markAnswer.js
@@ -1,15 +1,18 @@
 const { Ques, Ans, User, Tag } = require("../../Models");
-const { isSameUser } = require("./utilis");
+const { isSameUser, addScore } = require("./utilis");
+const {score} = require('../../config')
 const markAnswer = async (req, res) => {
 
    let err = false;
    try{
     const quesId = req.body.quesId;
     const answerId = req.body.answerId;
-    const ques = await Ques.findById(quesId, {'$set' : {satisfied : true}}).exec();
-    const answer = await Ans.findById(answerId, {'$set' : {marked : true}}).exec();
+    const ques = await Ques.findByIdAndUpdate(quesId, {'$set' : {satisfied : true}}).exec();
+    const answer = await Ans.findByIdAndUpdate(answerId, {'$set' : {marked : true}}).exec();
+
+    console.log("answer in mark ", answer)
     const user = await User.findById(answer.userId);
-    addScore(user, "totalScore", "satisfactoryAnswer")
+    addScore(user, "totalScore", score.satisfactoryAnswer)
     user.save();
    }
    catch(err){
@@ -26,7 +29,10 @@ const isQuestionAskedByUser = async(req, res) =>{
     let data = false;
     let err = false;
     try{
-    data = await isSameUser(req.body.quesId, req.user.id);
+    const user = await isSameUser(req.body.quesId, req.user.id);
+    const ques = await Ques.findById(req.body.quesId, "satisfied");
+    if(user && !ques.satisfied) 
+        data = true;
     }
     catch{
         err = true;
