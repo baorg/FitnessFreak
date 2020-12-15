8622bb37d7845f2ef6752c013b004132fe440e1d
Mudit Bisht <3145159Noobi7uke@users.noreply.github.com>
2020-12-15 20:36:08 +0530

    Push to Heroku

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,11 +1,5 @@
 const CONFIG = require('./config.json');
-require('dotenv').config({ path: '../.env' });
 
-console.log('CONFIG: ', process.env.NODE_ENV);
-
-if (process.env.NODE_ENV == 'production')
-    module.exports = CONFIG.PRODUCTION;
-else if (process.env.NODE_ENV == 'testing')
-    module.exports = CONFIG.TESTING
-else
-    module.exports = CONFIG.LOCAL
+module.exports = CONFIG.PRODUCTION;
+// module.exports = CONFIG.TESTING;
+//module.exports = CONFIG.LOCAL;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'server//.env' });
+// console.log('NODE ENV: ', process.env.CLOUDINARY_CLOUD_NAME);
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
