c851468acf929d37ce4ee6a9d285304702c273d8
MR.AV <akashvermaa0208@gmail.com>
2021-01-12 15:48:43 +0530

    delete questions and answers

---
+++ server/Handlers/Question/deleteQuestion.js
@@ -0,0 +1,43 @@
+
+const { Ques, Ans, User, Tag } = require("../../Models");
+
+function deleteQuestion(req, res){
+let err = false;
+const quesId = req.body.quesId;
+const userId = req.user.id;
+try{
+        await Ques.deleteOne({id : quesId})
+        const user = await User.findById(userId)
+        user.questions.pull(new ObjectId(quesId))
+        await user.save();
+  }
+  catch(err){
+          err = true;
+          console.log("err while deleting question ", err);
+  }
+  finally{
+          return res.send({err: err})
+  }
+
+}
+function deleteAnswer(req, res){
+        let err = false;
+        const ansId = req.body.ansId;
+        const userId = req.user.id;
+        try{
+                await Ans.deleteOne({id : ansId})
+                const user = User.findById(userId)
+                user.answers.pull(new ObjectId(ansId))
+                user.save();
+          }
+          catch(err){
+                  err = true;
+                  console.log("err while deleting question ", err);
+          }
+          finally{
+                  return res.send({err: err})
+          }
+        
+        }
+
+module.exports = {deleteQuestion, deleteAnswer}
--- server/Routers/question.js
+++ server/Routers/question.js
@@ -41,6 +41,9 @@ router.get("/getNotifications", isAuthenticated, QuestionHandler.getNotification
 router.get("/getQuestionsCategoryWise/:name", QuestionHandler.getQuestionsCategoryWise);
 router.post("/getAnswersByUserOnly/:quesId", QuestionHandler.getAnswersByUserOnly);
 
+router.post('/deleteQuestion', QuestionHandler.deleteQuestion)
+router.post('/deleteAnswer', QuestionHandler.deleteAnswer)
+
 router.post("/markAnswer", QuestionHandler.markAnswer)
 router.post("/isQuestionAskedByUser", QuestionHandler.isQuestionAskedByUser)
 router.get("/get-type/:name", QuestionHandler.getTypeOfQuestionsHandler);
